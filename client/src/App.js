f8d4969099f87051fe5be8db32955ff62fff9d9f
vomi-exe <mehtavomi@gmail.com>
2021-07-06 12:37:52 +0530

    client

---
+++ client/src/App.js
@@ -0,0 +1,82 @@
+
+import "./app.css";
+import axios from "axios";
+import { useState } from "react";
+import jwt_decode from "jwt-decode";
+
+function App() {
+  const [user, setUser] = useState(null);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+
+ 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      
+    } catch (err) {
+      
+    }
+  };
+
+  return (
+    <div className="container">
+      {user ? (
+        <div className="home">
+          <span>
+            Welcome to the <b>{user.isAdmin ? "admin" : "user"}</b> dashboard{" "}
+            <b>{user.username}</b>.
+          </span>
+          <span>Delete Users:</span>
+          <button className="deleteButton" onClick={() => handleDelete(1)}>
+            Delete John
+          </button>
+          <button className="deleteButton" onClick={() => handleDelete(2)}>
+            Delete Jane
+          </button>
+          {error && (
+            <span className="error">
+              You are not allowed to delete this user!
+            </span>
+          )}
+          {success && (
+            <span className="success">
+              User has been deleted successfully...
+            </span>
+          )}
+        </div>
+      ) : (
+        <div className="login">
+          <form onSubmit={handleSubmit}>
+            <span className="formTitle">Lama Login</span>
+            <input
+              type="text"
+              placeholder="username"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit" className="submitButton">
+              Login
+            </button>
+          </form>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
