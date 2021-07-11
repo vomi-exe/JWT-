5c0898eb7d1c1eb0e632f507b2badaad78c5965d
vomi-exe <mehtavomi@gmail.com>
2021-07-11 14:43:09 +0530

    ok

--- api/index.js
+++ api/index.js
@@ -192,4 +192,4 @@ if (process.env.NODE_ENV === "production") {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => console.log("Backend server is running!"));
+app.listen(PORT, () => console.log("Backend server is running!"));
